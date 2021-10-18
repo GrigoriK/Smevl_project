1d8f9e1fb5b02dd464b7804de2396eabe36b4d9f
GrigoriK <grigorij.kislyakov@yandex.ru>
2021-10-19 00:22:34 +0300

    Excel branch 1)Has added first controller for getting test excel file

--- src/main/java/smevel/controllers/ExcelController.java
+++ src/main/java/smevel/controllers/ExcelController.java
@@ -2,13 +2,13 @@ package smevel.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import smevel.services.impl.ExcelService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @Slf4j
@@ -18,9 +18,15 @@ public class ExcelController {
 
     private final ExcelService excelService;
 
-    @RequestMapping(path = "/emptyExcelVL/{type}", method = RequestMethod.GET)
-    public ResponseEntity createEmptyVL(@PathVariable("type") String type) {
-        return new ResponseEntity<>(excelService.getEmptyExcel(), HttpStatus.OK);
+    @RequestMapping(path = "/emptyExcelVL", method = RequestMethod.GET)
+    public void createEmptyVL(HttpServletResponse response) {
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=test.xlsx");
+        try {
+            response.getOutputStream().write(excelService.getEmptyExcel());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
